ac01df19c62e1bed00c4b42118c738d05c6f6d18
Ranjan Anantharaman <ranjan.anantharaman@gmail.com>
2019-08-16 18:07:41 -0400

    feature complete

--- src/INIBuilder.jl
+++ src/INIBuilder.jl
@@ -4,7 +4,6 @@ using REPL
 using REPL.TerminalMenus
 using Circuitscape
 
-include("state.jl")
 include("filepicker.jl")
 include("run.jl")
 
--- src/state.jl
+++
@@ -1,6 +0,0 @@
-mutable struct State
-    s::UInt32
-end
-
-forward!(s::State) = (s.s += 1; )
-back!(s::State) = (s.s -= 1;)
